7be0dbcbcc1a46db0e567bf99e2518cd34944a23
김현승 <corn120033@gmail.com>
2023-05-18 22:23:13 +0900

    2.2.1. 단순 연결 리스트 코드 업데이트

--- sample_code/SinglyLinkedList.cs
+++ sample_code/SinglyLinkedList.cs
@@ -15,12 +15,24 @@ public class SinglyLinkedList<T>
 {
     public Node<T> Head;
     public Node<T> Tail;
-    public int Count;
+    public int Count = 0;
 
-    public void Add(T Value)
+    public bool IsEmpty()
     {
-        Node<T> newNode = new Node<T>(Value);
-        if (Head == null)
+        if (Head == null || Tail == null || Count == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void Add(T value)
+    {
+        Node<T> newNode = new Node<T>(value);
+        if (IsEmpty())
         {
             Head = newNode;
             Tail = Head;
@@ -28,16 +40,153 @@ public class SinglyLinkedList<T>
         }
         else
         {
-            if(Count == 1)
+            if (Count == 1)
             {
                 Tail = newNode;
                 Head.NextNode = Tail;
-            } else
+            }
+            else
             {
                 Tail.NextNode = newNode;
                 Tail = newNode;
             }
             Count++;
+        }
+    }
+
+    public bool AddBefore(int index, T value)
+    {
+        if(!IsInvalidIndex(index))
+        {
+            return false;
+        } else
+        {
+            Node<T> newNode = new Node<T>(value);
+            Node<T> beforeNode = null;
+            Node<T> targetNode = Head;
+
+            for (int i = 0; i < index; i++)
+            {
+                beforeNode = targetNode;
+                targetNode = targetNode.NextNode;
+            }
+
+            if(targetNode == Head)
+            {
+                newNode.NextNode = Head;
+                Head = newNode;
+            } else
+            {
+                beforeNode.NextNode = newNode;
+                newNode.NextNode = targetNode;
+            }
+            Count++;
+            return true;
+        }
+    }
+
+    public bool AddAfter(int index, T value)
+    {
+        if (!IsInvalidIndex(index))
+        {
+            return false;
+        }
+        else
+        {
+            Node<T> newNode = new Node<T>(value);
+            Node<T> targetNode = Head;
+
+            for (int i = 0; i < index; i++)
+            {
+                targetNode = targetNode.NextNode;
+            }
+
+            if (targetNode == Tail)
+            {
+                Tail.NextNode = newNode;
+                Tail = newNode;
+            }
+            else
+            {
+                newNode.NextNode = targetNode.NextNode;
+                targetNode.NextNode = newNode;
+            }
+            Count++;
+            return true;
+        }
+    }
+
+    public bool Remove(int index)
+    {
+        if (IsEmpty())
+        {
+            return false;
+        }
+        else
+        {
+            Node<T> beforeNode = null;
+            Node<T> removeNode = Head;
+            for (int i = 0; i < index; i++)
+            {
+                beforeNode = removeNode;
+                removeNode = removeNode.NextNode;
+            }
+
+            if (Count == 1)
+            {
+                Head = null;
+                Tail = null;
+            }
+            else if (removeNode == Head)
+            {
+                Head = Head.NextNode;
+                removeNode = null;
+            }
+            else
+            {
+                beforeNode.NextNode = removeNode.NextNode;
+                removeNode = null;
+            }
+            Count--;
+            return true;
+        }
+    }
+
+    public Node<T> Peek()
+    {
+        return Head;
+    }
+
+    public int getCount()
+    {
+        return Count;
+    }
+
+    public Node<T> getNode(int index)
+    {
+        if (IsEmpty())
+        {
+            return null;
+        }
+        else
+        {
+            Node<T> selectedNode = Head;
+            for (int i = 0; i < index; i++)
+            {
+                selectedNode = selectedNode.NextNode;
+            }
+            return selectedNode;
+        }
+    }
+
+    private bool IsInvalidIndex(int index)
+    {
+        if(0 > index || getCount() - 1 < index)
+        {
+            return false;
+        } else
+        {
+            return true;
         }
     }
 }
